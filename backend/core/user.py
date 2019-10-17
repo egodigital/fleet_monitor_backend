c26c03a7972ae000dd99328dd6671439ffca71a7
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-18 01:17:30 +0200

    Add user gamification metrics

--- backend/core/user.py
+++ backend/core/user.py
@@ -10,11 +10,16 @@ class User:
     Class to store user data.s
     """
 
+    FEATURE_DISCOVERY_MAX = 10
+
     def __init__(self, name, phone_number, user_id, password):
         self.name = name
         self.phone_number = phone_number
         self.credentials = Credentials(user_id, password)
         self.tags = []
+        self.bonus = 0
+        self.feature_discovery = 0
+        self.free_rides = 0
 
     def set_phone_number(self, phone_number):
         self.phone_number = phone_number
@@ -36,3 +41,17 @@ class User:
             self.tags.remove(tag)
             return True
         return False
+
+    def add_bonus_points(self, bonus):
+        temp = self.bonus
+        if (temp % 100 + bonus) > 100:
+            self.free_rides += 1
+        self.bonus += bonus
+
+    def increment_feature_discovery(self):
+        self.feature_discovery += 1
+        if self.feature_discovery == self.FEATURE_DISCOVERY_MAX:
+            self.free_rides += 1
+
+    def take_free_ride(self):
+        self.free_rides -= 1
