5538743fd432b79dbd076bf822d44614af7c8e20
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-17 18:47:32 +0200

    Add core package with simple user data class

---
+++ backend/core/user.py
@@ -0,0 +1,8 @@
+class User:
+
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
