7e74cabc589334bbf7a6568239a1ac40fd8c1e50
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-17 18:51:56 +0200

    Extend user class by settings
    Settings are still to be defined

--- backend/core/user.py
+++ backend/core/user.py
@@ -1,8 +1,19 @@
-class User:
+class UserSettings:
+    pass
 
-    def __init__(self, name, phone_number):
+
+class User:
+    """
+    Class to store user data.s
+    """
+
+    def __init__(self, name, phone_number, settings=None):
         self.name = name
         self.phone_number = phone_number
+        self.settings = settings
 
     def set_phone_number(self, phone_number):
         self.phone_number = phone_number
+
+    def update_settings(self, settings):
+        self.settings = settings
