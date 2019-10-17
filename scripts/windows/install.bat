4c5262f37c26a095aadf435248765acbb8443fab
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-17 18:26:46 +0200

    Update windows installation script

--- scripts/windows/install.bat
+++ scripts/windows/install.bat
@@ -1,19 +1,21 @@
 # --- Virtual Python environment
-echo "Installing project dependendencies into virtual Python environment."
-set PYTHON_VERSION=3.6
-set PRJ_ROOT=%1
-set VENV_DIR=%PRJ_ROOT%\venv
+:: echo "Installing project dependendencies into virtual Python environment."
+:: set PYTHON_VERSION=3.6
+:: set PRJ_ROOT=%1
+:: set VENV_DIR=%PRJ_ROOT%\venv
 
+:: echo %PRJ_ROOT%
+:: echo %VENV_DIR%
+
+:: IF NOT EXIST %VENV_DIR% (
+::   virtualenv -p python%PYTHON_VERSION% %VENV_DIR%
+::)
+:: source %VENV_DIR%\Scripts\activate
+:: pip install -r %PRJ_ROOT%\requirements.txt
+
+:: # --- PYTHONPATH environment variable
+SET PRJ_ROOT=%~dp0
 echo %PRJ_ROOT%
-echo %VENV_DIR%
-
-IF NOT EXIST %VENV_DIR% (
-    virtualenv -p python%PYTHON_VERSION% %VENV_DIR%
-)
-source %VENV_DIR%\Scripts\activate
-pip install -r %PRJ_ROOT%\requirements.txt
-
-# --- PYTHONPATH environment variable
 set PYTHONPATH=%PYTHONPATH%;^
     %PRJ_ROOT%\backend;^
     %PRJ_ROOT%\backend\api;^
