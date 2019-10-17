e63c34e16d3ac80f922a37176217496dee9387e6
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-17 20:00:47 +0200

    Extend user class

--- backend/core/user.py
+++ backend/core/user.py
@@ -1,11 +1,29 @@
+class Credentials:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
 class User:
     """
     Class to store user data.s
     """
 
-    def __init__(self, name, phone_number):
+    def __init__(self, name, phone_number, group, username, password):
         self.name = name
         self.phone_number = phone_number
+        self.group = group
+        self.credentials = Credentials(username, password)
 
     def set_phone_number(self, phone_number):
         self.phone_number = phone_number
+
+    def set_group(self, group):
+        self.group = group
+
+    def set_username(self, username):
+        self.credentials.username = username
+
+    def set_password(self, password):
+        self.credentials.password = password
