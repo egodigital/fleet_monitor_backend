edaa4c74104f3d06ecc44b49aaa41d517d23e31b
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-17 18:09:59 +0200

    Update windows installation script

--- scripts/windows/install.bat
+++ scripts/windows/install.bat
@@ -2,14 +2,22 @@
 echo "Installing project dependendencies into virtual Python environment."
 set PYTHON_VERSION=3.6
 set PRJ_ROOT=%1
-set VENV_DIR=%PRJ_ROOT%/venv
+set VENV_DIR=%PRJ_ROOT%\venv
+
+echo %PRJ_ROOT%
+echo %VENV_DIR%
 
 IF NOT EXIST %VENV_DIR% (
     virtualenv -p python%PYTHON_VERSION% %VENV_DIR%
 )
-source %VENV_DIR%/Scripts/activate
-pip install -r %PRJ_ROOT%/requirements.txt
+source %VENV_DIR%\Scripts\activate
+pip install -r %PRJ_ROOT%\requirements.txt
 
 # --- PYTHONPATH environment variable
-echo "Adding PYTHONPATH to ~/.bashrc"
-python %PRJ_ROOT%/scripts/set_pythonpath.py windows
+set PYTHONPATH=%PYTHONPATH%;^
+    %PRJ_ROOT%\backend;^
+    %PRJ_ROOT%\backend\api;^
+    %PRJ_ROOT%\backend\api\swagger_client;^
+    %PRJ_ROOT%\backend\api\swagger_client\api;^
+    %PRJ_ROOT%\backend\api\swagger_client\models;^
+    %PRJ_ROOT%\backend\snippets;^
