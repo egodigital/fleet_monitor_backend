3014e9f6109b284b625b59762f5d191bfdcb9189
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-18 01:04:36 +0200

    Add further parameters to coar

--- backend/core/car.py
+++ backend/core/car.py
@@ -1,10 +1,21 @@
+from .assumptions import CAR_REACH_AT_FULL_CHARGE
+from .assumptions import CAR_REPAIR_TIME
+
+
 class Car:
 
-    def __init__(self, license_, model):
+    def __init__(self, license_: str, model: str):
         self.license = license
         self.model = model
         self.car_id = license + "_" + model
-        self.range = None
+        self.range = CAR_REACH_AT_FULL_CHARGE
+        self.in_repair = False
+        self.repair_time = 0
 
-    def set_range(self, range):
+    def set_range(self, range: float):
         self.range = range
+
+    def set_in_repair(self, in_repair):
+        if in_repair:
+            self.in_repair = True
+            self.repair_time = CAR_REPAIR_TIME
