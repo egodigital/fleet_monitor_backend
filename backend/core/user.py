726e0852e3998d06ea68a853661ac83027f6456d
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-17 19:12:23 +0200

    Delete settings from user class

--- backend/core/user.py
+++ backend/core/user.py
@@ -1,19 +1,11 @@
-class UserSettings:
-    pass
-
-
 class User:
     """
     Class to store user data.s
     """
 
-    def __init__(self, name, phone_number, settings=None):
+    def __init__(self, name, phone_number):
         self.name = name
         self.phone_number = phone_number
-        self.settings = settings
 
     def set_phone_number(self, phone_number):
         self.phone_number = phone_number
-
-    def update_settings(self, settings):
-        self.settings = settings
