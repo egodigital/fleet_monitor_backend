bddc2d93368651739920d5980bd9d97b58882f0e
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-18 03:36:34 +0200

    Update car module

--- backend/core/car.py
+++ backend/core/car.py
@@ -5,14 +5,14 @@ from .globals import CAR_REPAIR_TIME
 class Car:
 
     def __init__(self, license_: str, model: str):
-        self.license = license
+        self.license = license_
         self.model = model
         self.range = CAR_REACH_AT_FULL_CHARGE
         self.in_repair = False
         self.repair_time = 0
 
-    def set_range(self, range: float):
-        self.range = range
+    def set_range(self, range_: float):
+        self.range = range_
 
     def set_in_repair(self, in_repair):
         if in_repair:
