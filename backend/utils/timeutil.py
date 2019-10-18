65a2fb894e91aaeb29e68e7419fd7e45cb8a4582
Thomas Salzmann <thomas.salzmann.privat@gmail.com>
2019-10-18 05:32:59 +0200

    Extend timeutil

--- backend/utils/timeutil.py
+++ backend/utils/timeutil.py
@@ -3,18 +3,28 @@ import math
 
 from backend.core.globals import SMALLEST_TIME_UNIT
 
+MINUTE_TO_TIME_SLOT_FACTOR = 1 / SMALLEST_TIME_UNIT
+TIME_SLOT_TO_MINUTE_FACTOR = SMALLEST_TIME_UNIT
 DAY_TO_MINUTES_FACTOR = 1440
 HOUR_TO_MINUTE_FACTOR = 60
 SECOND_TO_MINUTE_FACTOR = 1/60
 MINUTE_TO_HOUR_FACTOR = 1/60
 
 
-def booking_slots_to_minutes(amount: int) -> int:
-    return amount * SMALLEST_TIME_UNIT
+def minutes_to_time_slots(amount: int) -> int:
+    return amount * MINUTE_TO_TIME_SLOT_FACTOR
 
 
-def minutes_to_booking_slots(amount: int) -> int:
-    return amount / SMALLEST_TIME_UNIT
+def time_slots_to_minutes(amount: int) -> int:
+    return amount * TIME_SLOT_TO_MINUTE_FACTOR
+
+
+def days_to_minutes(amount: int) -> int:
+    return amount * DAY_TO_MINUTES_FACTOR
+
+
+def hours_to_minutes(amount: float) -> int:
+    return int(amount * HOUR_TO_MINUTE_FACTOR)
 
 
 def seconds_to_minutes(amount: int) -> int:
@@ -25,14 +35,10 @@ def minutes_to_hours(amount: int) -> float:
     return amount * MINUTE_TO_HOUR_FACTOR
 
 
-def hours_to_minutes(amount: float) -> int:
-    return int(amount * HOUR_TO_MINUTE_FACTOR)
-
-
 def datetimes_to_time_slots(start_time: datetime, end_time:
                             datetime) -> int:
     delta = end_time - start_time
     seconds = delta.total_seconds()
     minutes = seconds_to_minutes(seconds)
-    booking_slots = minutes_to_booking_slots(minutes)
+    booking_slots = minutes_to_time_slots(minutes)
     return booking_slots
